a22f4910e9b3761b273c65fc8f817e4dd386d4a4
Duy <pham.duy@dena.jp>
2019-11-13 09:21:23 +0700

    Update menu

---
+++ assets/data/menu.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    link: '/',
+    label: 'Home'
+  },
+  {
+    link: '/lightning2d',
+    label: 'Lightning 2D'
+  }
+]
--- components/Menu.vue
+++ components/Menu.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div :class="{active: isActive}">
     <div class="menu">
       <div ref="menuIcon" class="icon">
         <span/>
@@ -8,11 +8,8 @@
       </div>
     </div>
     <ul class="nav">
-      <li>
-        <nuxt-link @click.native="toggleMenu" to="/">Home</nuxt-link>
-      </li>
-      <li>
-        <nuxt-link @click.native="toggleMenu" to="/lightning">Lightning</nuxt-link>
+      <li v-for="(item, index) in menus" :key="index">
+        <nuxt-link @click.native="toggleMenu" :to="item.link">{{item.label}}</nuxt-link>
       </li>
     </ul>
   </div>
@@ -20,24 +17,42 @@
 
 <script>
   const ACTIVE_CLASS = 'menu-active';
+  import MenuList from '@/assets/data/menu';
+
   export default {
     name: 'MenuComponent',
+
+    data() {
+      return {
+        isActive: false
+      }
+    },
+
+    computed: {
+      menus: () => MenuList
+    },
 
     mounted() {
       const icon = this.$refs.menuIcon;
       const wrapper = document.querySelector('.wrapper');
       icon.addEventListener('click', () => {
-        const isActive = wrapper.classList.contains(ACTIVE_CLASS);
-        if(isActive) wrapper.classList.remove(ACTIVE_CLASS);
-        else wrapper.classList.add(ACTIVE_CLASS);
+        this.handleWrapper();
+        this.isActive = !this.isActive;
       })
     },
 
     methods: {
       toggleMenu() {
+        this.handleWrapper();
+        this.isActive = !this.isActive;
+      },
+
+      handleWrapper() {
         const wrapper = document.querySelector('.wrapper');
-        wrapper.classList.remove(ACTIVE_CLASS);
-      }
+        if(this.isActive) wrapper.classList.remove(ACTIVE_CLASS);
+        else wrapper.classList.add(ACTIVE_CLASS);
+      },
+
     },
   }
 </script>
@@ -67,7 +82,9 @@
       top: 50%;
       left: 50%;
       transform: translate(-50%, -50%);
+      will-change: transform, width;
       cursor: pointer;
+      transition: all 300ms ease-out;
       &:nth-child(1) {
         transform: translate(-50%, -15px);
       }
@@ -97,4 +114,19 @@
     }
   }
 }
+.active {
+  .icon {
+    > span {
+      &:nth-child(1) {
+        width: 20px;
+        transform: rotate(32deg) translateY(-7px) translateX(-5px);
+      }
+       &:nth-child(3){
+         width: 20px;
+        transform: rotate(-32deg) translateY(3px) translateX(-2px);
+       }
+    }
+  }
+}
+
 </style>
